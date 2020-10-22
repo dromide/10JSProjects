3abd04a9ce3af6d7fa9323f6a7991b6f1b0265a5
dromide <mz.hisia@gmail.com>
2020-10-22 22:22:06 +0900

    weather app

---
+++ weather-app/script.js
@@ -0,0 +1,9 @@
+const APIURL = 'https://www.metaweather.com/api/';
+
+async function getWeatherByLocation(location) {
+  const resp = await fetch(APIURL + 'location/search/?query=' + location);
+  const respData = await resp.json();
+
+  console.log(respData);
+};
+getWeatherByLocation('London');
