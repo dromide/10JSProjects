352982a1fd1a37cf5a824b518bd688a39a1d1e8b
dromide <mz.hisia@gmail.com>
2020-10-23 15:26:34 +0900

    wrap it up

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,9 +1,52 @@
-const APIURL = 'https://www.metaweather.com/api/';
+const apikey = '3265874a2c77ae4a04bb96236a642d2f';
 
-async function getWeatherByLocation(location) {
-  const resp = await fetch(APIURL + 'location/search/?query=' + location);
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+  const resp = await fetch(url(city), {
+    origin: 'cors'
+  });
   const respData = await resp.json();
 
-  console.log(respData);
-};
-getWeatherByLocation('London');
+  console.log(respData, KtoC(respData.main.temp));
+
+  addWeatherToPage(respData);
+}
+// getWeatherByLocation('London');
+
+function addWeatherToPage(data) {
+  const temp = KtoC(data.main.temp);
+
+  const weather = document.createElement('div');
+  weather.classList.add('weather');
+
+  weather.innerHTML = `
+    <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />${temp}°C<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+    <small>${data.weather[0].main}</small>
+    `;
+  // <small>in ${search.value}</small>
+
+  //clean up
+  main.innerHTML = '';
+
+  main.appendChild(weather);
+}
+
+function KtoC(K) {
+  return (K - 273.15).toFixed(2);
+  // return Math.floor(K - 273.15);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const city = search.value;
+
+  if (city) {
+    getWeatherByLocation(city);
+  }
+});
